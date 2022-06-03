0182d2d83cbd343563bd9d14165671a30675c8a3
7alip <talipaltas@gmail.com>
2022-06-04 01:08:24 +0200

    Remove comment sorting option

--- src/components/shared/comment-list.js
+++ src/components/shared/comment-list.js
@@ -1,4 +1,4 @@
-import { FormControl, HStack, Select, Stack, Text } from '@chakra-ui/react'
+import { Stack, Text } from '@chakra-ui/react'
 import { useTranslation } from 'next-i18next'
 
 import { CommentItem } from '~components'
@@ -8,18 +8,10 @@ export const CommentList = ({ comments }) => {
 
   return (
     <Stack p={4} spacing={4} bg='white' boxShadow='base'>
-      <HStack justifyContent={'space-between'}>
-        <Text fontSize='lg' fontWeight='semibold'>
-          {t('apply-form.comments')}
-        </Text>
+      <Text fontSize='lg' fontWeight='semibold'>
+        {t('apply-form.comments')}
+      </Text>
 
-        <FormControl w='auto'>
-          <Select id='category'>
-            <option>Popular</option>
-            <option>Latest</option>
-          </Select>
-        </FormControl>
-      </HStack>
       <Stack spacing={4}>
         {comments?.map(comment => {
           return <CommentItem key={comment.id} comment={comment} />
