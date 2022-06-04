5ae9c2abb46b6c4987619de3df2a4e81628d9f2c
7alip <talipaltas@gmail.com>
2022-06-04 22:36:52 +0200

    Include other arts data to art query

--- src/services/art/find-one.js
+++ src/services/art/find-one.js
@@ -2,6 +2,27 @@ import { useQuery } from 'react-query'
 
 import { request } from '~lib'
 
+export const getArtsByCategories = async (locale, categories, id) => {
+  console.log('categories', categories)
+  try {
+    const response = await request({
+      url: 'api/arts',
+      locale,
+      filters: {
+        categories: { code: { $in: categories } },
+        id: { $ne: id },
+      },
+      populate: ['artist.user.avatar', 'categories', 'images', 'likes'],
+      sort: 'publishedAt:desc',
+      pageSize: 4,
+    })
+
+    return response?.result || []
+  } catch (error) {
+    console.log('error.respons', error.respons)
+  }
+}
+
 export const getArt = async (locale, slug) => {
   const response = await request({
     url: 'api/arts',
@@ -10,7 +31,17 @@ export const getArt = async (locale, slug) => {
     locale,
   })
 
-  return response.result?.[0] || null
+  const art = response.result?.[0]
+
+  if (!art) return null
+
+  const arts = await getArtsByCategories(
+    locale,
+    art.categories?.map(c => c.code),
+    art.id,
+  )
+
+  return { ...art, arts }
 }
 
 export const useGetArt = (locale, slug) =>
