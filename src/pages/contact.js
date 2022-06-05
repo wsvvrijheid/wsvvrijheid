6fef30ffd02d652e31d16065670e4ca3e67b8d35
7alip <talipaltas@gmail.com>
2022-06-05 05:43:16 +0200

    Update contact page design

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,6 @@
-import { Box, Button, Heading, Link, Stack, Text, VStack } from '@chakra-ui/react'
+import { Box, Button, Divider, Heading, Link, SimpleGrid, Stack, Text, VStack, Wrap } from '@chakra-ui/react'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'react-i18next'
-import { FaWhatsapp as FaWhatsapp } from 'react-icons/fa'
 import { MdEmail, MdLocationOn, MdPhone } from 'react-icons/md'
 
 import { ContactForm, Container, Layout, SocialButtons } from '~components'
@@ -12,56 +11,42 @@ const Contact = ({ seo }) => {
   return (
     <Layout seo={seo}>
       <Box minH='inherit'>
-        <Container minH='inherit' maxW='container.xl'>
-          <Stack justify='center' align='center' spacing={8} direction={{ base: 'column', lg: 'row' }} minH='inherit'>
+        <Container minH='inherit'>
+          <SimpleGrid my={{ base: 8, lg: 0 }} gap={8} alignContent='center' columns={{ base: 1, lg: 2 }} minH='inherit'>
             <VStack
               bg='blue.900'
+              color='blue.50'
               borderRadius='lg'
               p={{ base: 8, lg: 16 }}
-              w={{ base: 'full', lg: '500px' }}
               textAlign='center'
               justify='center'
-              spacing={{ base: 8, lg: 16 }}
+              spacing={8}
             >
-              <Box>
-                <Heading>{t('contact.title')}</Heading>
-                <Text mt={{ sm: 3, md: 3, lg: 5 }} color='blue.50'>
-                  {t('contact.fill-form')}
-                </Text>
-              </Box>
-              <VStack alignItems='flex-start' color='blue.50'>
+              <Heading textTransform='uppercase' fontWeight='black' as='h2' size='lg' color='blue.200'>
+                Stichting <br /> Wees de Stem voor Vrijheid
+              </Heading>
+              <Divider borderColor='whiteAlpha.400' />
+
+              <Wrap spacing={4} justify='center'>
                 <Button
                   as={Link}
                   isExternal
-                  borderWidth={2}
-                  borderColor='transparent'
-                  variant='ghost'
-                  _hover={{ borderColor: 'blue.500' }}
-                  leftIcon={<Box as={MdPhone} color='blue.500' size='20px' />}
+                  variant='link'
+                  color='blue.50'
+                  _hover={{ color: 'blue.200' }}
+                  leftIcon={<Box as={MdPhone} color='blue.200' size='20px' />}
                   href='tel:+31685221308'
                 >
                   +31-6 85221308
                 </Button>
+
                 <Button
                   as={Link}
                   isExternal
-                  borderWidth={2}
-                  borderColor='transparent'
-                  variant='ghost'
-                  _hover={{ borderColor: 'blue.500' }}
-                  leftIcon={<Box as={FaWhatsapp} color='blue.500' size='20px' />}
-                  href='https://api.whatsapp.com/send?phone=31685221308'
-                >
-                  {t('contact.form.contactOnWhatsApp')}
-                </Button>
-                <Button
-                  as={Link}
-                  isExternal
-                  borderWidth={2}
-                  borderColor='transparent'
-                  variant='ghost'
-                  _hover={{ borderColor: 'blue.500' }}
-                  leftIcon={<Box as={MdEmail} color='blue.500' size='20px' />}
+                  variant='link'
+                  color='blue.50'
+                  _hover={{ color: 'blue.200' }}
+                  leftIcon={<Box as={MdEmail} color='blue.200' size='20px' />}
                   href='mailto:info@wsvvrijheid.nl'
                 >
                   info@wsvvrijheid.nl
@@ -69,23 +54,53 @@ const Contact = ({ seo }) => {
                 <Button
                   as={Link}
                   isExternal
-                  borderWidth={2}
-                  borderColor='transparent'
-                  variant='ghost'
-                  _hover={{ borderColor: 'blue.500' }}
-                  leftIcon={<Box as={MdLocationOn} color='blue.500' size='20px' />}
+                  variant='link'
+                  color='blue.50'
+                  _hover={{ color: 'blue.200' }}
+                  leftIcon={<Box as={MdLocationOn} color='blue.200' size='20px' />}
                   href='https://goo.gl/maps/E9HaayQnXmphUWtN8'
+                  textAlign='left'
                 >
-                  Tandersplein 1, 3027 CN <br /> Rotterdam, Netherland
+                  Tandersplein 1, 3027 CN, Rotterdam
                 </Button>
-              </VStack>
+              </Wrap>
 
               <SocialButtons />
+
+              <Stack w='full' spacing={4}>
+                <Stack w='full'>
+                  <Text color='blue.200' fontWeight='semibold'>
+                    Yonetim Kurulu
+                  </Text>
+                  <Wrap justify='space-around' spacing={4}>
+                    <Box>
+                      <Text fontSize='sm'>Baskan</Text>
+                      <Text>Davut Dur</Text>
+                    </Box>
+                    <Box>
+                      <Text fontSize='sm'>Sayman</Text>
+                      <Text>Ibrahim Ataman</Text>
+                    </Box>
+                  </Wrap>
+                </Stack>
+
+                <Divider borderColor='whiteAlpha.400' />
+
+                <Wrap justify='space-around' fontSize='sm' textAlign='left'>
+                  <Text>KVK: </Text>
+                  <Text>RSIN: </Text>
+                </Wrap>
+              </Stack>
             </VStack>
-            <Box w={{ base: 'full', lg: '500px' }} bg='white' rounded='lg' p={{ base: 8, lg: 16 }} shadow='lg'>
+
+            <Stack rounded='lg' p={{ base: 8, lg: 16 }} shadow='lg'>
+              <Stack>
+                <Heading>{t('contact.title')}</Heading>
+                <Text>{t('contact.fill-form')}</Text>
+              </Stack>
               <ContactForm />
-            </Box>
-          </Stack>
+            </Stack>
+          </SimpleGrid>
         </Container>
       </Box>
     </Layout>
