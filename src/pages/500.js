36bdf985dcd1547bf6a34b6e36005650aff23956
7alip <talipaltas@gmail.com>
2022-06-16 06:52:41 +0200

    Add 404 and error pages

--- src/pages/500.js
+++ src/pages/500.js
@@ -1,5 +1,28 @@
-import { Layout } from '~components'
+import { Box, Center } from '@chakra-ui/react'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-export default function Home() {
-  return <Layout>Error</Layout>
+import { Container, Layout } from '~components'
+
+export default function ErrorPage() {
+  return (
+    <Layout>
+      <Center h='60vh' color='red.400'>
+        <Container>
+          <Box textAlign='center' fontWeight='black' fontSize='5xl'>
+            Error!
+          </Box>
+        </Container>
+      </Center>
+    </Layout>
+  )
+}
+
+export const getStaticProps = async context => {
+  const { locale } = context
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+  }
 }
