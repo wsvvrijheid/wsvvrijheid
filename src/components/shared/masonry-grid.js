76b307e166798032e9d642ecd5e4512f1a9531bf
7alip <talipaltas@gmail.com>
2022-06-17 08:11:48 +0200

    Update ArtCardSkeleton design

--- src/components/shared/masonry-grid.js
+++ src/components/shared/masonry-grid.js
@@ -12,7 +12,7 @@ export const MasonryGrid = ({ children, gap = 4, cols = [1, 2, 3, 4] }) => {
       columnClassName='masonry-grid_column'
       sx={{
         '& .masonry-grid_column': {
-          '&:not(:nth-child(1))': {
+          '&:not(:nth-of-type(1))': {
             bgClip: 'padding-box',
             pl: { base: cols[0] === 1 ? 0 : gap, lg: gap },
           },
